482a8a71bdebcd42ad8d509763baf3b283cb9d50
yanisCC <961990599@qq.com>
2025-06-17 21:31:16 +0800

    fix 添加注释

--- Realtime Import Of Battle Simulation.user.js
+++ Realtime Import Of Battle Simulation.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         [MWI] Real-time Import Of Battle Simulation
+// @name         [MWI] Realtime Import Of Battle Simulation
 // @name:zh-CN   [银河奶牛]战斗模拟实时导入
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Battle simulation imports the real-time configuration of the current character.
+// @version      0.0.1
+// @description  Battle simulation imports the realtime configuration of the current character.
 // @description:zh-CN  战斗模拟导入当前角色实时配置
 // @icon         https://www.milkywayidle.com/favicon.svg
 // @author       Yannis
-// @license      MIT
+// @license      CC-BY-NC-SA-4.0
 // @match        https://www.milkywayidle.com/*
 // @match        https://test.milkywayidle.com/*
 // @match        https://shykai.github.io/mwisim.github.io/*
@@ -15,23 +15,24 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // ==/UserScript==
- 
- 
+
+
 (function () {
     'use strict';
- 
+
     let playerId;
     let clientData = {};
- 
+
     hookWS();
- 
+
+    // 监听WebSocket
     function hookWS() {
         const dataProperty = Object.getOwnPropertyDescriptor(MessageEvent.prototype, "data");
         const oriGet = dataProperty.get;
- 
+
         dataProperty.get = hookedGet;
         Object.defineProperty(MessageEvent.prototype, "data", dataProperty);
- 
+
         function hookedGet() {
             const socket = this.currentTarget;
             if (!(socket instanceof WebSocket)) {
@@ -40,18 +41,20 @@
             if (socket.url.indexOf("api.milkywayidle.com/ws") <= -1 && socket.url.indexOf("api-test.milkywayidle.com/ws") <= -1) {
                 return oriGet.call(this);
             }
- 
+
             const message = oriGet.call(this);
             Object.defineProperty(this, "data", { value: message }); // Anti-loop
- 
+
             return handleMessage(message);
         }
     }
- 
+
+    // 获取客户端初始化数据
     function getInitClientData() {
         return JSON.parse(GM_getValue("init_client_data", ""));
     }
- 
+
+    // 获取当前角色数据
     function getCurrentPlayerData() {
         let playersDataStr = GM_getValue("init_character_data", "");
         if (playersDataStr) {
@@ -61,7 +64,8 @@
             return;
         }
     }
- 
+
+    // 获取角色数据
     function getPlayerData(id) {
         let playersDataStr = GM_getValue("mwi_players_data", null) || JSON.stringify(new Array());
         let playersData = JSON.parse(playersDataStr);
@@ -72,18 +76,20 @@
             return;
         }
     }
- 
+
+    // 保存角色数据
     function saveCharacterData(obj) {
         let playersDataStr = GM_getValue("mwi_players_data", null) || JSON.stringify(new Array());
         let playersData = JSON.parse(playersDataStr);
         playersData = playersData.filter(e => e.character.id !== obj.character.id);
         playersData.unshift(obj);
-        if (playersData.length > 20) {
+        if (playersData.length > 5) {
             playersData.pop();
         }
         GM_setValue("mwi_players_data", JSON.stringify(playersData));
     }
- 
+
+    // 消息处理
     function handleMessage(message) {
         let obj = JSON.parse(message);
         if (!obj) {
@@ -172,18 +178,18 @@
             }
         }
     }
- 
+
     // 添加实时导入按钮
     function addImportButtonForMWICombatSimulate() {
         const checkElem = () => {
             const btnImport = document.querySelector(`button#buttonImportExport`);
             if (btnImport) {
                 clearInterval(timer);
- 
+
                 let divRow = document.createElement("div");
                 divRow.className = "row";
                 btnImport.parentElement.parentElement.append(divRow);
- 
+
                 // 导入按钮
                 let div1 = document.createElement("div");
                 div1.className = "col-md-auto mb-2 pt-3";
@@ -204,21 +210,21 @@
         };
         let timer = setInterval(checkElem, 200);
     }
- 
+
     // 导入数据
     async function importDataForMWICombatSimulate(button) {
         clientData = getInitClientData();
         let player = getCurrentPlayerData();
- 
+
         const BLANK_PLAYER_JSON_STR = `{\"player\":{\"attackLevel\":1,\"magicLevel\":1,\"powerLevel\":1,\"rangedLevel\":1,\"defenseLevel\":1,\"staminaLevel\":1,\"intelligenceLevel\":1,\"equipment\":[]},\"food\":{\"/action_types/combat\":[{\"itemHrid\":\"\"},{\"itemHrid\":\"\"},{\"itemHrid\":\"\"}]},\"drinks\":{\"/action_types/combat\":[{\"itemHrid\":\"\"},{\"itemHrid\":\"\"},{\"itemHrid\":\"\"}]},\"abilities\":[{\"abilityHrid\":\"\",\"level\":\"1\"},{\"abilityHrid\":\"\",\"level\":\"1\"},{\"abilityHrid\":\"\",\"level\":\"1\"},{\"abilityHrid\":\"\",\"level\":\"1\"},{\"abilityHrid\":\"\",\"level\":\"1\"}],\"triggerMap\":{},\"zone\":\"/actions/combat/fly\",\"simulationTime\":\"100\",\"houseRooms\":{\"/house_rooms/dairy_barn\":0,\"/house_rooms/garden\":0,\"/house_rooms/log_shed\":0,\"/house_rooms/forge\":0,\"/house_rooms/workshop\":0,\"/house_rooms/sewing_parlor\":0,\"/house_rooms/kitchen\":0,\"/house_rooms/brewery\":0,\"/house_rooms/laboratory\":0,\"/house_rooms/observatory\":0,\"/house_rooms/dining_room\":0,\"/house_rooms/library\":0,\"/house_rooms/dojo\":0,\"/house_rooms/gym\":0,\"/house_rooms/armory\":0,\"/house_rooms/archery_range\":0,\"/house_rooms/mystical_study\":0}}`;
- 
+
         const playerNames = {};
         const imported = {};
         const battleData = {};
         let isParty = false;
         let zone = "/actions/combat/fly";
         let isZoneDungeon = false;
-        
+
         if (!player?.partyInfo?.partySlotMap) {
             // 个人
             playerNames[0] = player.character.name;
@@ -263,7 +269,7 @@
             zone = player.partyInfo?.party?.actionHrid;
             isZoneDungeon = clientData.actionDetailMap[zone]?.combatZoneInfo?.isDungeon;
         }
- 
+
         // Select zone or dungeon
         if (zone) {
             if (isZoneDungeon) {
@@ -288,7 +294,7 @@
                 }
             }
         }
- 
+
         for (let i = 1; i <= 5; i++) {
             if (!battleData[i]) {
                 battleData[i] = BLANK_PLAYER_JSON_STR;
@@ -301,15 +307,15 @@
                 document.querySelector(`input#player${i}.form-check-input.player-checkbox`).dispatchEvent(new Event("change"));
             }
         }
- 
+
         document.querySelector(`a#group-combat-tab`).click();
         const editImport = document.querySelector(`input#inputSetGroupCombatAll`);
         editImport.value = JSON.stringify(battleData);
         document.querySelector(`button#buttonImportSet`).click();
- 
+
         // 模拟时长
         document.querySelector(`input#inputSimulationTime`).value = 24;
- 
+
         button.textContent = "成功导入数据";
         button.className = "btn btn-success";
         setTimeout(() => {
@@ -323,14 +329,15 @@
             }, 500);
         }
     }
- 
+
+    // 监听模拟结果
     async function observeResultsForMWICombatSimulate() {
         let resultDiv = document.querySelector(`div.row`)?.querySelectorAll(`div.col-md-5`)?.[2]?.querySelector(`div.row > div.col-md-5`);
         while (!resultDiv) {
             await new Promise((resolve) => setTimeout(resolve, 100));
             resultDiv = document.querySelector(`div.row`)?.querySelectorAll(`div.col-md-5`)?.[2]?.querySelector(`div.row > div.col-md-5`);
         }
- 
+
         const deathDiv = document.querySelector(`div#simulationResultPlayerDeaths`);
         const expDiv = document.querySelector(`div#simulationResultExperienceGain`);
         const consumeDiv = document.querySelector(`div#simulationResultConsumablesUsed`);
@@ -340,7 +347,7 @@
         expDiv.style.color = "black";
         consumeDiv.style.backgroundColor = "#F0F8FF";
         consumeDiv.style.color = "black";
- 
+
         let div = document.createElement("div");
         div.id = "tillLevel";
         div.style.backgroundColor = "#FFFFE0";
@@ -348,7 +355,7 @@
         div.textContent = "";
         resultDiv.append(div);
     }
- 
+
     // 构建战斗模拟信息
     function buildBattleObjFromInitData(obj) {
         let battleObj = {};
@@ -461,21 +468,21 @@
         }
         return battleObj;
     }
- 
+
     if (localStorage.getItem("initClientData")) {
         const obj = JSON.parse(localStorage.getItem("initClientData"));
         GM_setValue("init_client_data", localStorage.getItem("initClientData"));
- 
+
         clientData.actionDetailMap = obj.actionDetailMap;
         clientData.levelExperienceTable = obj.levelExperienceTable;
         clientData.itemDetailMap = obj.itemDetailMap;
         clientData.actionCategoryDetailMap = obj.actionCategoryDetailMap;
         clientData.abilityDetailMap = obj.abilityDetailMap;
     }
- 
+
     if (document.URL.includes("shykai.github.io/MWICombatSimulatorTest/")) {
         addImportButtonForMWICombatSimulate();
         observeResultsForMWICombatSimulate();
     }
- 
+
 })();
